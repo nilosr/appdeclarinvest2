82a6c01e8909cd0421349cd008603d35a37d1d40
nilosr <nilosromero@hotmail.com>
2022-06-18 15:15:27 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [totalInvestido, setTotalInvestido] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className=" w-screen">
+        <div className="flex items-center justify-evenly mt-10 mb-10">
+          <div className="text-2xl font-semibold">DeclarInvest</div>
+          {/* TODO:Icone carteira*/}
+          <button className="rounded-full px-7 py-7 bg-red-500" />
+        </div>
+        <div className="bg-blue-200 py-2">
+          <div className="text-lg  w-screen flex  ">
+            <div className="w-1/4" />
+            <div className="font-medium text-gray-600">Total Investido</div>
+          </div>
+          <div className="text-2xl flex justify-center text-blue-600 font-bold">
+            R$: {totalInvestido}
+          </div>
+        </div>
+        <div className="mt-7 flex justify-center items-center">
+          <div className="px-4 w-1/3 flex justify-end">
+            {/* TODO:Icone sino */}
+            <div className="rounded-full w-4 h-4 bg-gray-500"></div>
+          </div>
+          <div className="text-xl font-semibold text-gray-800">Anotar</div>
+          <div className=" w-1/3"></div>
+        </div>
+        <div className="flex justify-evenly mt-4">
+          {" "}
+          <button className="bg-green-400 rounded-lg w-28 h-9 text-xl shadow-md items-center text-gray-800 font-semibold">
+            Compra
+          </button>
+          <button className="bg-red-600 rounded-lg w-28 h-9 text-xl shadow-md items-center text-white font-semibold">
+            Compra
+          </button>
+        </div>
+        <div className="mt-7 flex justify-center items-center ">
+          <div className="w-1/3 px-4 flex justify-end">
+            {/* TODO:Icone papel */}
+            <div className="rounded-full w-4 h-4 bg-gray-500"></div>
+          </div>
+          <div className="text-xl font-semibold text-gray-800">Relatório</div>
+          <div className="w-1/3"></div>
+        </div>
+        <div className="flex justify-center">
+          <div className="block space-y-5 mt-4 w-3/5">
+            {/* TODO:Icones baixar */}
+            <button className="bg-blue-400 rounded-lg w-full h-10 text-xl shadow-md items-center text-white font-semibold">
+              Baixar csv
+            </button>
+            <button className="bg-blue-800 rounded-lg w-full h-10 text-xl shadow-md items-center text-white font-semibold">
+              Baixar PDF
+            </button>
+          </div>
+        </div>
+        <div className="bg-gray-800 w-screen text-white text-center font-semibold font-mono bottom-0 py-2 absolute ">
+          Desenvolvido por Nilosr
+        </div>
+      </div>
+    </>
   );
 }
 
